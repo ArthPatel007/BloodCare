6ecd8ae3e601db24d995ac13821a96f7588dbc25
Patel Arth <111559578+ArthPatel007@users.noreply.github.com>
2023-12-17 01:40:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -11,6 +11,6 @@ $conn = new mysqli($servername, $username, $password, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//  else {
-//     echo "connection is ok";
-// }
+ else {
+    echo "connection is ok";
+}
